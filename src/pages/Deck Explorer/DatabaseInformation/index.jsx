e9bfb92cc7fb04fd3dbeebb9960c4bcf8dae1ad2
Harris <zimibot@gmail.com>
2024-02-22 03:42:43 +0700

    Auto commit

--- src/pages/Deck Explorer/DatabaseInformation/index.jsx
+++ src/pages/Deck Explorer/DatabaseInformation/index.jsx
@@ -12,7 +12,8 @@ import { useLocation, useNavigate } from "@solidjs/router"
 import { Check, Close, CoPresent, ContentCopy, CopyAll } from "@suid/icons-material"
 import { Drawer } from "@suid/material"
 import { api } from "../../../helper/_helper.api"
-
+import RenderData from "./renderData"
+import { Loading } from "../../../component/loading"
 
 const DatabaseInformation = () => {
     const [__, { update }] = useAppState()
@@ -329,6 +330,81 @@ const DatabaseInformation = () => {
         ));
     }
 
+    const CardFrameData = () => {
+        const [limit, setLimit] = createSignal(3);
+        const [isLoadingData, setIsLoadingData] = createSignal(false);
+        const [isDataEmpty, setisDataEmpty] = createSignal(false);
+        let containerRef = null;
+
+        const handleScroll = () => {
+            const container = containerRef;
+            if (container.scrollLeft <= 0) {
+                // Jika scroll ke kiri
+                setIsLoadingData(false);
+            } else if ((container.scrollLeft + container.clientWidth + 1) >= container.scrollWidth) {
+                // Jika scroll ke kanan dan mencapai akhir
+
+                if (limit() === checkData().length) {
+                    setIsLoadingData(false)
+                    setisDataEmpty(true)
+                } else {
+                    setIsLoadingData(true);
+                    setTimeout(() => {
+                        if (limit() < checkData().length) {
+                            // Tambah limit jika data masih tersedia
+                            setLimit(prevLimit => prevLimit + 3);
+                            setIsLoadingData(false)
+                        } else {
+                            setLimit(checkData().length);
+                            setIsLoadingData(false); // Jika semua data sudah dimuat
+                        }
+                    }, 300);
+
+                }
+
+
+            } else {
+                setisDataEmpty(false)
+                setIsLoadingData(false)
+            }
+        };
+
+        createEffect(() => {
+            const container = containerRef;
+            if (container) {
+                container.addEventListener('scroll', handleScroll);
+            }
+
+            onCleanup(() => {
+                if (container) {
+                    container.removeEventListener('scroll', handleScroll);
+                }
+            });
+        });
+
+        return (
+            <CardFrame isLoading={isLoading} count={checkData} title={`INFORMATION category`} className="flex flex-col flex-1 relative">
+                <div id="container" ref={container => containerRef = container} className="absolute top-0 h-full  flex flex-1 flex-col w-full left-0 px-4 overflow-auto py-4 gap-2">
+                    <div className="flex gap-4 flex-1">
+                        {checkData().slice(checkData().length === 1 ? checkData().length - 1 : checkData().length - limit(), checkData().length).map((d, i) => (
+                            <RenderData key={i} b={d} k={i} checkItems={checkItems} Tags={Tags} IconButton={IconButton} ContentCopy={ContentCopy} CheckboxItems={CheckboxItems} FormControlLabel={FormControlLabel} checkData={checkData} setCheck={setCheck} setCheckAll={setCheckAll} onCopy={onCopy} mode={mode} saved={saved} />
+                        ))}
+                    </div>
+                </div>
+
+                <div className="absolute right-[20px] bottom-[20px] flex items-center justify-center">
+                    <div className="relative flex items-center gap-2 justify-center bg-primarry-2 px-4 py-2">
+                        <span>
+
+                            {limit()}  / {checkData().length} TOTAL
+                        </span>
+                        <CircularProgress sx={{ display: isLoadingData() ? "flex" : "none" }} size="16px"></CircularProgress>
+                    </div>
+                </div>
+            </CardFrame>
+        );
+    };
+
 
     return <ContainerPages>
         <div className="py-6 flex flex-col flex-1">
@@ -571,96 +647,7 @@ const DatabaseInformation = () => {
                     <div className="xl:col-span-6 flex-1 px-4 py-2 flex flex-col">
                         <Tags label={"MULTI SOURCE DATABASE INFORMATION"}></Tags>
 
-                        <CardFrame isLoading={isLoading} count={checkData} title={`INFORMATION category`} className="flex flex-col flex-1 relative">
-                            {<div className="absolute top-0 h-full  flex flex-1 flex-col w-full left-0 px-4 overflow-auto py-4 gap-2">
-                                <div className="flex gap-4 flex-1">
-                                    {checkData()?.map((b, k) => {
-                                        return <div className={` bg-[#1e1e1e] p-2 ${k === 0 ? " col-span-full" : checkData().length === 2 ? "col-span-full" : ""}`}>
-                                            <div className="border border-primarry-2 px-4 bg-primarry-1  z-50 flex justify-between items-center">
-                                                <Tags label={<span>DATA FROM <b>{b.label}</b></span>}></Tags>
-                                                <div>
-                                                    <CheckboxItems
-                                                        title="Checked All ITEMS"
-                                                        checked={b.checkAll} onChange={(a) => {
-                                                            setCheckAll(b.id, !a.target.checked)
-                                                        }}
-                                                    />
-                                                </div>
-                                            </div>
-                                            {b.data.map((x) => {
-                                                return <div className={`flex gap-4 relative border-b  ${mode() === "dark" ? "border-[#333]" : "border-[#aaa]"}  py-2 `}>
-                                                    <div className="flex gap-4 items-start flex-1 relative">
-                                                        <div className={`${mode() === "dark" ? "text-[#aaa]" : "text-[#444]"} sticky top-[10px] whitespace-nowrap w-[200px] z-10 px-4 pt-2`}> {x.total_data === 1 ? "" : `[${x.total_data}]`} {x.label}</div>
-                                                        <div className="flex-1 ">
-                                                            <div className="gap-2 flex  px-4 items-center">
-                                                                {x.data.map((d) => {
-                                                                    return <div title={d.label} className="flex items-center gap-2">
-                                                                        <FormControlLabel
-                                                                            class={`pl-4 !m-0 ${mode() === "dark" ? ` ${saved().isErrorMsg ? "border-red-500 text-red-500" : "border-[#454545] bg-[#2C2C2C]"}` : saved().isErrorMsg ? "border-red-500 text-red-500" : "bg-gray-200 text-[#444] border-[#aaa]"} pr-2 py-1 flex gap-4 border-[0] max-w-sm`}
-                                                                            checked={d.active}
-                                                                            onChange={(c) => checkItems(b.id, d.id, c.target.checked)}
-
-                                                                            label={<div className={`whitespace-nowrap max-w-xs relative ${x.label === "ID CARD PHOTO" ? "hover:z-50  hover:scale-[2.5] transition-all" : ""}`}>
-                                                                                <div className={x.label !== "ID CARD PHOTO" ? "text-ellipsis overflow-hidden relative" : "z-50"}>
-                                                                                    {x.label !== "ID CARD PHOTO" ? d.label : <div>
-                                                                                        <img className="w-20" src={d.label} />
-                                                                                    </div>}
-                                                                                </div>
-                                                                            </div>} labelPlacement="end" control={<CheckboxItems />} />
-
-                                                                        {x.label !== "ID CARD PHOTO" && <IconButton onClick={() => onCopy(d.label)} color="primary" size="small">
-                                                                            <ContentCopy fontSize="small"></ContentCopy>
-                                                                        </IconButton>}
-                                                                    </div>
-                                                                })}
-
-
-                                                            </div>
-                                                        </div>
-                                                        <div className="sticky top-[10px] pr-4">
-                                                            {x.data.length !== 1 && <CheckboxItems
-                                                                title="Checked All"
-                                                                checked={x.active} onChange={() => {
-                                                                    setCheck(prev => prev.map(z => {
-                                                                        return {
-                                                                            ...z,
-                                                                            data: z.data.map(s => {
-                                                                                if (x.id === s.id) {
-                                                                                    const shouldBeActive = !x.active;
-                                                                                    return {
-                                                                                        ...s,
-                                                                                        active: shouldBeActive,
-                                                                                        data: s.data.map(o => ({
-                                                                                            ...o,
-                                                                                            active: shouldBeActive
-                                                                                        }))
-                                                                                    }
-                                                                                }
-
-                                                                                return s
-                                                                            })
-                                                                        }
-                                                                    }))
-
-
-                                                                }}
-                                                            />}
-
-                                                        </div>
-                                                    </div>
-                                                    <div className="w-full flex justify-between items-center absolute bottom-[-5px] left-0">
-                                                        <div className={`h-2 w-2 ${mode() === "dark" ? "bg-[#222222]" : "bg-[#aaa]"} left-0`}></div>
-                                                        <div className={`h-2 w-2 ${mode() === "dark" ? "bg-[#222222]" : "bg-[#aaa]"} right-0`}></div>
-                                                    </div>
-                                                </div>
-
-                                            })}
-                                        </div>
-                                    })}
-                                </div>
-                            </div>}
-
-                        </CardFrame>
+                        <CardFrameData></CardFrameData>
                     </div>
                 </div>
             </CardBox>
---
+++ src/pages/Deck Explorer/DatabaseInformation/renderData.jsx
@@ -0,0 +1,95 @@
+
+// Definisikan fungsi komponen di luar fungsi utama
+const RenderData = ({ checkData, checkItems, setCheckAll, setCheck, onCopy, mode, saved, CheckboxItems, FormControlLabel, ContentCopy, IconButton, Tags, b, k }) => {
+  // Kembalikan hasil pemetaan data ke elemen yang diperlukan
+
+
+  return <div className={`bg-[#1e1e1e] p-2 ${k === 0 ? " col-span-full" : checkData().length === 2 ? "col-span-full" : ""}`}>
+    <div className="border border-primarry-2 px-4 bg-primarry-1  z-50 flex justify-between items-center">
+      <Tags label={<span>DATA FROM <b>{b.label}</b></span>}></Tags>
+      <div>
+        <CheckboxItems
+          title="Checked All ITEMS"
+          checked={b.checkAll}
+          onChange={(a) => {
+            setCheckAll(b.id, !a.target.checked)
+          }}
+        />
+      </div>
+    </div>
+    {b.data.map((x) => {
+      return (
+        <div className={`flex gap-4 relative border-b ${mode() === "dark" ? "border-[#333]" : "border-[#aaa]"} py-2 `}>
+          <div className="flex gap-4 items-start flex-1 relative">
+            <div className={`${mode() === "dark" ? "text-[#aaa]" : "text-[#444]"} sticky top-[10px] whitespace-nowrap w-[200px] z-10 px-4 pt-2`}> {x.total_data === 1 ? "" : `[${x.total_data}]`} {x.label}</div>
+            <div className="flex-1 ">
+              <div className="gap-2 flex  px-4 items-center">
+                {x.data.map((d) => {
+                  return (
+                    <div title={d.label} className="flex items-center gap-2">
+                      <FormControlLabel
+                        class={`pl-4 !m-0 ${mode() === "dark" ? ` ${saved().isErrorMsg ? "border-red-500 text-red-500" : "border-[#454545] bg-[#2C2C2C]"}` : saved().isErrorMsg ? "border-red-500 text-red-500" : "bg-gray-200 text-[#444] border-[#aaa]"} pr-2 py-1 flex gap-4 border-[0] max-w-sm`}
+                        checked={d.active}
+                        onChange={(c) => checkItems(b.id, d.id, c.target.checked)}
+                        label={
+                          <div className={`whitespace-nowrap max-w-xs relative ${x.label === "ID CARD PHOTO" ? "hover:z-50  hover:scale-[2.5] transition-all" : ""}`}>
+                            <div className={x.label !== "ID CARD PHOTO" ? "text-ellipsis overflow-hidden relative" : "z-50"}>
+                              {x.label !== "ID CARD PHOTO" ? d.label : <div><img className="w-20" src={d.label} /></div>}
+                            </div>
+                          </div>
+                        }
+                        labelPlacement="end"
+                        control={<CheckboxItems />}
+                      />
+                      {x.label !== "ID CARD PHOTO" &&
+                        <IconButton onClick={() => onCopy(d.label)} color="primary" size="small">
+                          <ContentCopy fontSize="small"></ContentCopy>
+                        </IconButton>
+                      }
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="sticky top-[10px] pr-4">
+              {x.data.length !== 1 &&
+                <CheckboxItems
+                  title="Checked All"
+                  checked={x.active}
+                  onChange={() => {
+                    setCheck(prev => prev.map(z => {
+                      return {
+                        ...z,
+                        data: z.data.map(s => {
+                          if (x.id === s.id) {
+                            const shouldBeActive = !x.active;
+                            return {
+                              ...s,
+                              active: shouldBeActive,
+                              data: s.data.map(o => ({
+                                ...o,
+                                active: shouldBeActive
+                              }))
+                            };
+                          }
+                          return s;
+                        })
+                      };
+                    }));
+                  }}
+                />
+              }
+            </div>
+          </div>
+          <div className="w-full flex justify-between items-center absolute bottom-[-5px] left-0">
+            <div className={`h-2 w-2 ${mode() === "dark" ? "bg-[#222222]" : "bg-[#aaa]"} left-0`}></div>
+            <div className={`h-2 w-2 ${mode() === "dark" ? "bg-[#222222]" : "bg-[#aaa]"} right-0`}></div>
+          </div>
+        </div>
+      );
+    })}
+  </div>
+};
+
+// Export fungsi agar dapat diakses dari luar
+export default RenderData;
