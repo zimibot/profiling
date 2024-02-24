8539d9be24186807c5f6b28e3dc3f3721fe8995f
Harris Munahar <zimibot@gmail.com>
2024-02-25 00:50:36 +0700

    Auto commit

--- src/helper/_helper.api.jsx
+++ src/helper/_helper.api.jsx
@@ -2,49 +2,59 @@ import axios from "axios";
 import Swal from "sweetalert2";
 
 export const api = () => {
+  const instance = axios.create({
+    baseURL:
+      process.env.NODE_ENV === "production"
+        ? "https://k17tech.com/users"
+        : "http://localhost:3000/users",
+    timeout: 30000,
+  });
 
+  // Alter defaults after instance has been created
+  instance.defaults.headers.common["Authorization"] =
+    localStorage.getItem("token");
+  instance.defaults.headers.common["Content-Type"] = "application/json";
 
-    const instance = axios.create({
-        baseURL: process.env.NODE_ENV === 'production' ? 'https://k17tech.com/users' : "http://localhost:3000/users",
-        timeout: 30000
-    });
+  instance.interceptors.request.use(
+    function (config) {
+      // Do something before request is sent
+      return config;
+    },
+    function (error) {
+      console.log(error, "rest");
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
+  // Add a response interceptor
 
-    // Alter defaults after instance has been created
-    instance.defaults.headers.common['Authorization'] = localStorage.getItem("token");
-    instance.defaults.headers.common['Content-Type'] = "application/json";
+  instance.interceptors.response.use(
+    function (response) {
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      // Do something with response data
+      return response;
+    },
+    function (error) {
+      if (error?.code !== "ERR_BAD_REQUEST") {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Session Expired",
+          timer: 5000,
+          didClose: () => {
+            localStorage.removeItem("token");
+            setTimeout(() => {
+              window.location.reload();
+            }, 100);
+          },
+        });
+      }
 
-    instance.interceptors.request.use(function (config) {
-        // Do something before request is sent
-        return config;
-    }, function (error) {
-        console.log(error)
-        // Do something with request error
-        return Promise.reject(error);
-    });
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      return Promise.reject(error);
+    }
+  );
 
-    // Add a response interceptor
-    instance.interceptors.response.use(function (response) {
-        // Any status code that lie within the range of 2xx cause this function to trigger
-        // Do something with response data
-        return response;
-    }, function (error) {
-        if (error?.code !== "ERR_BAD_REQUEST") {
-
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Session Expired',
-                didClose: () => {
-                    localStorage.removeItem("token")
-                    window.location.reload()
-                }
-            })
-        }
-
-        // Any status codes that falls outside the range of 2xx cause this function to trigger
-        // Do something with response error
-        return Promise.reject(error);
-    });
-
-    return instance
-}
+  return instance;
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,33 +1,36 @@
-import { Navigate, useLocation } from "@solidjs/router"
-import BreadcrumbsPages from "../component/breadcrumbs"
-import { useAppState } from "../helper/_helper.context"
-import { createEffect } from "solid-js"
-
+import { Navigate, useLocation } from "@solidjs/router";
+import BreadcrumbsPages from "../component/breadcrumbs";
+import { useAppState } from "../helper/_helper.context";
+import { createEffect } from "solid-js";
+import Swal from "sweetalert2";
+import { api } from "../helper/_helper.api";
 const ContainerPages = (props) => {
-    const [appStore, { update }] = useAppState()
+  const [appStore, { update }] = useAppState();
 
-    let location = useLocation()
+  let location = useLocation();
 
 
-    createEffect(() => {
-        if (location.pathname.split("/")[3] !== "database-information") {
-            update(d => ({...d,  dataSearch: [], terkait: null }))
-        } 
-    })
-
-    if (!appStore().token) {
-        return <Navigate href={"/"}></Navigate>
+  createEffect(() => {
+    if (location.pathname.split("/")[3] !== "database-information") {
+      update((d) => ({ ...d, dataSearch: [], terkait: null }));
     }
+  });
 
-    return <div className="p-4 flex flex-1 flex-col">
-        <BreadcrumbsPages></BreadcrumbsPages>
-        {props.children}
-        {/* <div className="flex fixed w-full h-full z-30 top-0  justify-center items-center left-0 backdrop-blur">
+  if (!appStore().token) {
+    return <Navigate href={"/"}></Navigate>;
+  }
+
+  return (
+    <div className="p-4 flex flex-1 flex-col">
+      <BreadcrumbsPages></BreadcrumbsPages>
+      {props.children}
+      {/* <div className="flex fixed w-full h-full z-30 top-0  justify-center items-center left-0 backdrop-blur">
             <div className="flex items-center gap-2 bg-primarry-1 p-2">
                 Loading Data... <CircularProgress size={21}></CircularProgress>
             </div>
         </div> */}
     </div>
-}
+  );
+};
 
-export default ContainerPages
+export default ContainerPages;
