33d95533f73a85199d0cb0a91ba40b46e4e641a2
Harris Munahar <zimibot@gmail.com>
2024-03-19 18:25:31 +0700

    Auto commit

--- src/component/headers/_menu/index.jsx
+++ src/component/headers/_menu/index.jsx
@@ -12,13 +12,13 @@ export const Menu = () => {
             <div className={`border-b py-2 ${mode() === "dark" ? "border-b-[#222222]" : "border-b-[#aaa]"}  relative z-50`}>
                 <div className="flex  flex-wrap lg:gap-[25px]  items-center sm:flex-col lg:flex-row">
                     <div className="mb-4 lg:mb-0 text-[40px] w-32">
-                      <img src={logo}></img>
+                        <img className="w-full" src={logo}></img>
                     </div>
                     {appStore().token ? <>
                         <MenuTabs></MenuTabs>
                         <div className="flex-1 relative h-[53px] hidden lg:block">
                             <div className={`${mode() === "dark" ? "bg-primarry-1" : "bg-gray-200"} h-full w-full absolute top-0 flex items-center justify-end px-4`}>
-                               
+
                             </div>
                         </div>
                     </> : ""}
