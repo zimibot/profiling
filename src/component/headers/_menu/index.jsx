c5accf63e88339418aac0b32ee9a09682646665f
Harris Munahar <zimibot@gmail.com>
2024-03-24 14:22:11 +0700

    Auto commit

--- src/component/headers/_menu/index.jsx
+++ src/component/headers/_menu/index.jsx
@@ -25,7 +25,7 @@ export const Menu = () => {
         <div className={`p-3 sticky top-0 z-10 ${mode() === "dark" ? "bg-[#0D0D0D]" : ""}`}>
             <div className={`border-b py-2 ${mode() === "dark" ? "border-b-[#222222]" : "border-b-[#aaa]"}  relative z-50`}>
                 <div className="flex  flex-wrap lg:gap-[25px]  items-center sm:flex-col lg:flex-row">
-                    <div className="mb-4 lg:mb-0 text-[40px] w-32">
+                    <div className="mb-4 lg:mb-0 text-[40px] w-48">
                         <img className="w-full" src={logo}></img>
                     </div>
                     {appStore().token ? <>
