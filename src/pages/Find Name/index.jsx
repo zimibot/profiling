996f8360183ffd177e054996f1c9c77d3b22da83
Harris Munahar <zimibot@gmail.com>
2024-03-28 04:10:22 +0700

    Auto commit

--- src/pages/Find Name/index.jsx
+++ src/pages/Find Name/index.jsx
@@ -6,19 +6,23 @@ import { SearchForm } from "../Deck Explorer/searchFrom"
 import { api } from "../../helper/_helper.api"
 import { useLocation } from "@solidjs/router"
 import { Loading } from "../../component/loading"
-import { Button } from "@suid/material"
+import { Button, Dialog, DialogContent } from "@suid/material"
+import { Close } from "@suid/icons-material"
 
 const FindName = () => {
     const [data, setData] = createSignal()
     const [isLoading, setIsloading] = createSignal()
     const [detail, setDetail] = createSignal()
-
+    const [showPopup, setShowPopup] = createSignal()
+    const [isloadingDetail, setisLoadingDetail] = createSignal()
     const onDetail = (id) => {
+        setisLoadingDetail(true)
+        setDetail()
         api().get(`/deck-explorer/sna-data-more?type=id_data&keyword=${id}`).then(s => {
             console.log(s.data.items)
 
             let columnsDetail = []
-
+            setisLoadingDetail(false)
             for (const key in s.data.items) {
                 columnsDetail.push(key)
             }
@@ -28,6 +32,7 @@ const FindName = () => {
             })
         })
     }
+
 
     const columns = [
         {
@@ -69,7 +74,39 @@ const FindName = () => {
             setIsloading(false)
         })
     })
+
+    const onViewGambar = (e) => {
+        setShowPopup({
+            status: true,
+            src: e.target.src
+        })
+
+        setisLoadingDetail(true)
+    }
+
+    const onClosePopup = () => {
+        setShowPopup()
+    }
+
     return <ContainerPages>
+        <Dialog
+
+            open={showPopup()?.status}
+            onClose={onClosePopup}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogContent sx={{
+                background: "#222"
+            }}>
+                <div className="absolute right-0 top-0 p-4">
+                    <Button onClick={onClosePopup} variant="contained" color="warning">
+                        <Close></Close>
+                    </Button>
+                </div>
+                <img className="w-full object-contain" src={showPopup()?.src}></img>
+            </DialogContent>
+        </Dialog>
         <div className="py-4 flex gap-2 flex-1">
             <div className="flex flex-col gap-2 w-[800px]">
                 <SearchForm></SearchForm>
@@ -81,13 +118,19 @@ const FindName = () => {
 
                 </div>
             </div>
-            <CardBox title="Result">
-                <div className="grid grid-cols-2 gap-2">
-                    {detail() ? detail().columns.map(a => (
-                        <div className="fle bg-primarry-2 p-2">
-                            <div className="w-36">{a}</div>
-                            <div>{a === "ID CARD PHOTO" ? <img className="w-[100px]" src={`data:image/jpeg;base64,${detail().data[a]}`}></img> : detail().data[a]}</div>
-                        </div>)) : ""}
+            <CardBox title="Result" className="flex-1 flex flex-col">
+                <div className="flex flex-col flex-1 relative">
+
+                    <div className="absolute w-full h-full top-0 left-0">
+                        {isloadingDetail() && <Loading></Loading>}
+                        <div className="grid grid-cols-2 gap-2 relative h-full w-full overflow-auto">
+                            {detail() ? detail().columns.map(a => (
+                                <div className="fle bg-primarry-2">
+                                    <div className=" font-bold p-2  bg-blue-400 w-full">{a}</div>
+                                    <div className="p-2">{a === "ID CARD PHOTO" || a === "FOTO" ? <Button><img onClick={onViewGambar} className="w-[100px]" src={`data:image/jpeg;base64,${detail().data[a]}`}></img></Button> : detail().data[a]}</div>
+                                </div>)) : <div className="flex items-center justify-center w-full h-full absolute bottom-0 top-0">Detail Not found</div>}
+                        </div>
+                    </div>
                 </div>
             </CardBox>
         </div>
