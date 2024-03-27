68bb304a73590f9ae183a0a4695e151ba7e017d2
Harris Munahar <zimibot@gmail.com>
2024-03-28 05:29:11 +0700

    Auto commit

--- src/pages/Face Finder/gallery.jsx
+++ src/pages/Face Finder/gallery.jsx
@@ -7,18 +7,21 @@ export const GalleryData = () => {
     const [data, setData] = createSignal()
 
     createEffect(() => {
-        api().get("/deck-explorer/gallery").then(a => {
-            console.log(a)
+        api().get("/deck-explorer/gallery?page=2").then(a => {
             setData(a.data)
         })
     })
-    return <CardBox title="GALLERY">
-        <div className="grid grid-cols-4">
-            {data() ? data().items.map(a => {
-                return <Button>
-                    <img src={a.baseurl}></img>
-                </Button>
-            }) : ""}
+    return <CardBox title="GALLERY" className="flex flex-col flex-1">
+        <div className="relative flex-1">
+            <div className="w-full h-full absolute left-0 top-0 overflow-auto">
+                <div className="grid grid-cols-4">
+                    {data() ? data().items.map(a => {
+                        return <Button>
+                            <img src={a.baseurl}></img>
+                        </Button>
+                    }) : ""}
+                </div>
+            </div>
         </div>
     </CardBox>
 }
