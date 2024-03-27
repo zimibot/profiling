7a7e78f2333c6b27d951b04706b9f0b1aae63beb
Harris Munahar <zimibot@gmail.com>
2024-03-27 18:51:29 +0700

    Auto commit

--- src/pages/Find Name/index.jsx
+++ src/pages/Find Name/index.jsx
@@ -1,10 +1,14 @@
 import ContainerPages from ".."
+import { CardBox } from "../../component/cardBox"
 import { SearchForm } from "../Deck Explorer/searchFrom"
 
 const FindName = () => {
     return <ContainerPages>
-        <div className="py-4">
+        <div className="py-4 flex gap-2 flex-1">
             <SearchForm></SearchForm>
+            <CardBox title="Result">
+
+            </CardBox>
         </div>
     </ContainerPages>
 }
