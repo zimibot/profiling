e1b8120e41060486c1308f36555242c879d14508
Harris Munahar <zimibot@gmail.com>
2024-03-27 20:45:26 +0700

    Auto commit

--- src/component/empty/index.jsx
+++ src/component/empty/index.jsx
@@ -1,11 +1,11 @@
 import { LibraryBooks } from "@suid/icons-material";
 
 export const Empty = (props) => {
-    return <div className={`absolute w-full px-4 py-6 flex justify-center ${props.className ? props.className : ""}`}>
+    return <div className={`absolute w-full px-4 py-6 flex justify-center h-full ${props.className ? props.className : ""}`}>
         <div className="flex flex-col items-center gap-2">
-            <LibraryBooks sx={{
+            {/* <LibraryBooks sx={{
                 fontSize: 55
-            }}></LibraryBooks>
+            }}></LibraryBooks> */}
             <span> Empty Data</span>
         </div>
     </div>
--- src/pages/Find Name/index.jsx
+++ src/pages/Find Name/index.jsx
@@ -5,32 +5,57 @@ import { Tables } from "../../component/tables"
 import { SearchForm } from "../Deck Explorer/searchFrom"
 import { api } from "../../helper/_helper.api"
 import { useLocation } from "@solidjs/router"
+import { Loading } from "../../component/loading"
 
 const FindName = () => {
-    const [data, setData] = createSignal({
-        items: [
-            {
-                category: "test"
-            }
-        ]
-    })
+    const [data, setData] = createSignal()
+    const [isLoading, setIsloading] = createSignal()
+
+    const columns = [
+        {
+            label: "Nama Lengkap",
+            name: "NAMA_LGKP",
+        },
+        {
+            label: "Tanggal Lahir",
+            name: "DESC_TGL_LHR",
+        },
+        {
+            label: "Nama Kota",
+            name: "NAMA_KAB",
+        },
+        {
+            label: "Pekerjaan",
+            name: "PEKERJAAN",
+        },
+        {
+            label: "Save",
+            name: "function",
+        },
+    ]
 
     const location = useLocation()
 
     createEffect(() => {
+        setIsloading(true)
         const path = location.pathname.split("/").pop()
         api().get(`/deck-explorer/search_name?keyword=${path}`).then(a => {
-            console.log(a)
+            setData({
+                items: a.data.items
+            })
+            setIsloading(false)
         })
     })
     return <ContainerPages>
         <div className="py-4 flex gap-2 flex-1">
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 w-[800px]">
                 <SearchForm></SearchForm>
                 <div className="flex-1 relative">
                     <div className="absolute w-full h-full top-0 left-0 flex flex-col">
-                        <Tables data={data}></Tables>
+                        <Tables columns={columns} data={data}></Tables>
                     </div>
+                    {isLoading() && <Loading></Loading>}
+
                 </div>
             </div>
             <CardBox title="Result">
